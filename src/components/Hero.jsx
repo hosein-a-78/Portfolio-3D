625002c29e2387def006f0fd84803f418315ddf5
abed-a-78 <abed.re.k00021@gmail.com>
2024-05-08 16:12:20 +0330

    write page Hero and Computers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+// import { mation } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas/Computers'
 
 const Hero = () => {
     return (
-        <div>
-
-        </div>
+        <section className='relative w-full h-screen mx-auto'>
+            <div className={`${styles.paddingX} absolute inset-0 top-[120x] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+                <div className='flex flex-col justify-center items-center mt-5'>
+                    <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+                    <div className='w-1 sm:h-80 h-40 violet-gradient' />
+                </div>
+                <div>
+                    <h1 className={`${styles.heroHeadText} text-white`}>Hi , I'
+                        <span className='text-[#915eff]'>Hosein</span>
+                    </h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        Ideveloper 3D visuals, user
+                        <br className='sm:block hidden' />
+                        interfaces and web applications
+                    </p>
+                </div>
+            </div>
+            <ComputersCanvas />
+        </section>
     );
 };
 
